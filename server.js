91a2863aa1c6d1a0d795aae1563764a5632b60d0
Satya438 <satyabodapti008@gmial.com>
2023-05-17 16:42:37 +0530

    aftd

--- server.js
+++ server.js
@@ -75,4 +75,14 @@ app.delete("/deletestudentdetails/:id", async (req, res) => {
 
 app.listen(3000, () => console.log("server running..."));
 
+module.exports = (req, res) => {
+  // Set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle the request
+  // ...
+};
+
 module.exports = app;
