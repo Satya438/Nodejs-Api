1b6cf8eae1f8d50bf0deb66c339140046d965cb5
Satya438 <satyabodapti008@gmial.com>
2023-05-18 15:46:20 +0530

    added cors

--- upload.js
+++ upload.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const router = require("./route");
 app.use("/", router);
@@ -7,3 +8,12 @@ app.use("/", router);
 app.listen(3000, () => {
   console.log(`Application listening on http://localhost:${3000}`);
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+module.exports = app;
